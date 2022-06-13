b2dce1295842ce46edf4a803e5d033df436992d9
Jinesh Patel <c0851605@mylambton.ca>
2022-06-13 16:58:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lab2_jinesh_patel_c0851605_android/Home.java
@@ -0,0 +1,27 @@
+package com.example.lab2_jinesh_patel_c0851605_android;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.lab2_jinesh_patel_c0851605_android.databinding.ActivityHomeBinding;
+
+public class Home extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
